e116447e228b66d33aa03d6f0b1d5cf723ba2941
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-22 23:50:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 set -e
 # make data
 if [ -z "`ls /letsencrypt`" ]; then cp -R /letsencrypt-start/* /letsencrypt; fi
-# ssh
-if [ ! -f "/runssh.sh" ]; then /runssh.sh; fi
 # cron
 service cron start
 exec "$@"
