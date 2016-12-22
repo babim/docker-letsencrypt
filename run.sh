aaec3bbaf6f997b786b87f9040836ec07aea9b57
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-22 23:40:57 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+# make data
+if [ -z "`ls /letsencrypt`" ]; then cp -R /letsencrypt-start/* /letsencrypt; fi
+# ssh
+if [ ! -f "/runssh.sh" ]; then /runssh.sh; fi
+# cron
+service cron start
+exec "$@"
